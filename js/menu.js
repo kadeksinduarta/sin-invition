a057d63a07f6edc092012c40abd6409c6e2a380c
kadeksinduarta <ikadeksinduarta@gmail.com>
2025-03-30 21:26:22 +0800

    membuat dickon

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,46 @@
 // menu
+
+const menuDiHalamanUtama = [
+  {
+    id: 16,
+    name: "Elegant 2",
+    category: "Elegant",
+    price: 150000,
+    image: "gambar/tema-Elegant-2.png",
+    link: "https://s-invitation.my.id/elegant-2/?untuk=Nama+Tamu",
+  },
+  {
+    id: 17,
+    name: "Elegant 3",
+    category: "Elegant",
+    price: 150000,
+    image: "gambar/tema-Elegant-3.png",
+    link: "https://s-invitation.my.id/elegant-3/?untuk=Nama+Tamu",
+  },
+  {
+    id: 18,
+    name: "Elegant 4",
+    category: "Elegant",
+    price: 150000,
+    image: "gambar/tema-Elegant-4.png",
+    link: "https://s-invitation.my.id/elegant-4/?untuk=Nama+Tamu",
+  },
+  {
+    id: 19,
+    name: "Elegant 5",
+    category: "Elegant",
+    price: 150000,
+    image: "gambar/tema-Elegant-5.png",
+    link: "https://s-invitation.my.id/elegant-5/?untuk=Nama+Tamu",
+  },
+];
+
 const themes = [
   {
     id: 1,
     name: "Tema 1",
     category: "Tema",
-    price: 200000,
+    price: 150000,
     image: "../gambar/template-1.png",
     link: "https://s-invitation.my.id/tema-1/?untuk=Nama+Tamu",
   },
@@ -12,7 +48,7 @@ const themes = [
     id: 2,
     name: "Tema 2",
     category: "Tema",
-    price: 200000,
+    price: 150000,
     image: "../gambar/template-2.png",
     link: "https://s-invitation.my.id/tema-2/?untuk=Nama+Tamu",
   },
@@ -20,7 +56,7 @@ const themes = [
     id: 3,
     name: "Tema 3",
     category: "Tema",
-    price: 200000,
+    price: 150000,
     image: "../gambar/template-3.png",
     link: "https://s-invitation.my.id/tema-3/?untuk=Nama+Tamu",
   },
@@ -28,7 +64,7 @@ const themes = [
     id: 4,
     name: "Tema 4",
     category: "Tema",
-    price: 200000,
+    price: 150000,
     image: "../gambar/template-4.png",
     link: "https://s-invitation.my.id/tema-4/?untuk=Nama+Tamu",
   },
@@ -36,7 +72,7 @@ const themes = [
     id: 5,
     name: "Tema 5",
     category: "Tema",
-    price: 200000,
+    price: 150000,
     image: "../gambar/template-5.png",
     link: "https://s-invitation.my.id/tema-5/?untuk=Nama+Tamu",
   },
@@ -44,7 +80,7 @@ const themes = [
     id: 6,
     name: "Tema 6",
     category: "Tema",
-    price: 200000,
+    price: 150000,
     image: "../gambar/template-6.png",
     link: "https://s-invitation.my.id/tema-6/?untuk=Nama+Tamu",
   },
@@ -52,7 +88,7 @@ const themes = [
     id: 7,
     name: "Tema 7",
     category: "Tema",
-    price: 200000,
+    price: 150000,
     image: "../gambar/template-7.png",
     link: "https://s-invitation.my.id/tema-7/?untuk=Nama+Tamu",
   },
@@ -60,7 +96,7 @@ const themes = [
     id: 8,
     name: "Tema 8",
     category: "Tema",
-    price: 200000,
+    price: 150000,
     image: "../gambar/template-8.png",
     link: "https://s-invitation.my.id/tema-8/?untuk=Nama+Tamu",
   },
@@ -68,7 +104,7 @@ const themes = [
     id: 9,
     name: "Tema 9",
     category: "Tema",
-    price: 200000,
+    price: 150000,
     image: "../gambar/template-9.png",
     link: "https://s-invitation.my.id/tema-9/?untuk=Nama+Tamu",
   },
@@ -76,7 +112,7 @@ const themes = [
     id: 10,
     name: "Tema 10",
     category: "Tema",
-    price: 200000,
+    price: 150000,
     image: "../gambar/Tema-10.png",
     link: "https://s-invitation.my.id/tema-10/?untuk=Nama+Tamu",
   },
@@ -84,7 +120,7 @@ const themes = [
     id: 11,
     name: "Tema 11",
     category: "Tema",
-    price: 200000,
+    price: 150000,
     image: "../gambar/Tema-11.png",
     link: "https://s-invitation.my.id/tema-11/?untuk=Nama+Tamu",
   },
@@ -92,7 +128,7 @@ const themes = [
     id: 12,
     name: "Tema 12",
     category: "Tema",
-    price: 200000,
+    price: 150000,
     image: "../gambar/Tema-12.png",
     link: "https://s-invitation.my.id/tema-12/?untuk=Nama+Tamu",
   },
@@ -100,7 +136,7 @@ const themes = [
     id: 13,
     name: "Monochrome",
     category: "Tema",
-    price: 200000,
+    price: 150000,
     image: "../gambar/monochrome.png",
     link: "https://s-invitation.my.id/monochrome/?untuk=Nama+Tamu",
   },
@@ -108,7 +144,7 @@ const themes = [
     id: 14,
     name: "Cream Elegant",
     category: "Tema",
-    price: 200000,
+    price: 150000,
     image: "../gambar/Cream-Elegant.png",
     link: "https://s-invitation.my.id/cream-elegant/?untuk=Nama+Tamu",
   },
@@ -116,7 +152,7 @@ const themes = [
     id: 15,
     name: "Elegant 1",
     category: "Elegant",
-    price: 200000,
+    price: 150000,
     image: "../gambar/tema-Elegant-1.png",
     link: "https://s-invitation.my.id/elegant-1/?untuk=Nama+Tamu",
   },
@@ -124,7 +160,7 @@ const themes = [
     id: 16,
     name: "Elegant 2",
     category: "Elegant",
-    price: 200000,
+    price: 150000,
     image: "../gambar/tema-Elegant-2.png",
     link: "https://s-invitation.my.id/elegant-2/?untuk=Nama+Tamu",
   },
@@ -132,7 +168,7 @@ const themes = [
     id: 17,
     name: "Elegant 3",
     category: "Elegant",
-    price: 200000,
+    price: 150000,
     image: "../gambar/tema-Elegant-3.png",
     link: "https://s-invitation.my.id/elegant-3/?untuk=Nama+Tamu",
   },
@@ -140,7 +176,7 @@ const themes = [
     id: 18,
     name: "Elegant 4",
     category: "Elegant",
-    price: 200000,
+    price: 150000,
     image: "../gambar/tema-Elegant-4.png",
     link: "https://s-invitation.my.id/elegant-4/?untuk=Nama+Tamu",
   },
@@ -148,7 +184,7 @@ const themes = [
     id: 19,
     name: "Elegant 5",
     category: "Elegant",
-    price: 200000,
+    price: 150000,
     image: "../gambar/tema-Elegant-5.png",
     link: "https://s-invitation.my.id/elegant-5/?untuk=Nama+Tamu",
   },
@@ -159,7 +195,7 @@ const themesTanpaFoto = [
     id: 1,
     name: "Tanpa Foto Tema 1",
     category: "Tanpa Foto",
-    price: 55000,
+    price: 100000,
     image: "../gambar/tanpa-foto-tema-1.png",
     link: "https://s-invitation.my.id/tanpa-foto-tema-1/?untuk=nama+tamu",
   },
@@ -167,20 +203,15 @@ const themesTanpaFoto = [
     id: 2,
     name: "Tanpa Foto Tema 2",
     category: "Tanpa Foto",
-    price: 55000,
+    price: 100000,
     image: "../gambar/tanpa-foto-tema-2.png",
     link: "https://s-invitation.my.id/tanpa-foto-tema-2/?untuk=nama+tamu",
   },
 ];
 
 document.addEventListener("DOMContentLoaded", () => {
-  const container = document.querySelector(".card-demo.include-foto .paket");
-  const btnTanpaFoto = document.querySelector("#btnTanpaFoto");
-  const btnIncludeFoto = document.querySelector("#btnIncludeFoto");
-  const btnBerurutan = document.querySelector("#btnBerurutan");
-  const btnTerbaru = document.querySelector("#btnTerbaru");
-  const btnElegant = document.querySelector("#temaElegant");
-  btnBerurutan.style.backgroundColor = "#00cff3";
+  const container = document.querySelector(".demo-undangan .paket");
+
   function renderThemes(data) {
     container.innerHTML = "";
     data.forEach((theme) => {
@@ -193,7 +224,44 @@ document.addEventListener("DOMContentLoaded", () => {
               <div class="contentt">
                 <h2>${theme.name}</h2>
                 <p>Harga</p>
-                <p class="harga">Rp.${theme.price.toLocaleString()}</p>
+                <h3 class="harga">Rp.${theme.price.toLocaleString()}</h3>
+                <p class="harga-diskon">Rp. ${(theme.price * 0.5).toLocaleString()}</p>
+              </div>
+              <div class="button-preview">
+                <h1>Lihat</h1>
+              </div>
+            </div>
+          </a>
+        `;
+    });
+  }
+
+  renderThemes(menuDiHalamanUtama);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const container = document.querySelector(".card-demo.include-foto .paket");
+  const btnTanpaFoto = document.querySelector("#btnTanpaFoto");
+  const btnIncludeFoto = document.querySelector("#btnIncludeFoto");
+  const btnBerurutan = document.querySelector("#btnBerurutan");
+  const btnTerbaru = document.querySelector("#btnTerbaru");
+  const btnElegant = document.querySelector("#temaElegant");
+  btnBerurutan.style.backgroundColor = "#00cff3";
+
+  function renderThemes(data) {
+    container.innerHTML = "";
+    data.forEach((theme) => {
+      container.innerHTML += `
+          <a href="${theme.link}" target="_blank">
+            <div class="gambar">
+              <img src="${theme.image}" alt="${theme.name}" />
+            </div>
+            <div class="deskripsi">
+              <div class="contentt">
+                <h2>${theme.name}</h2>
+                <p>Harga</p>
+                <h3 class="harga">Rp.${theme.price.toLocaleString()}</h3>
+                <p class="harga-diskon">Rp. ${(theme.price * 0.5).toLocaleString()}</p>
               </div>
               <div class="button-preview">
                 <h1>Lihat</h1>
